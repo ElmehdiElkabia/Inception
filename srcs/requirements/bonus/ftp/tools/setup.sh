692ee65eb399f6efaf21facea9e1ad475c26eae1
elmehdi elkabia <elmehdielkabia@gmail.com>
2026-08-01 17:51:57 +0100

    Add FTP service setup with vsftpd in Docker configuration

---
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+echo "=> Starting FTP setup..."
+
+mkdir -p /var/run/vsftpd/empty
+chmod 755 /var/run/vsftpd
+chmod 555 /var/run/vsftpd/empty
+
+if ! id "${FTP_USER}" >/dev/null 2>&1; then
+    echo "=> Creating user ${FTP_USER}..."
+
+    useradd \
+        -m \
+        -d /var/www/html \
+        -s /bin/bash \
+        -G www-data \
+        "${FTP_USER}"
+fi
+
+usermod -aG www-data "${FTP_USER}"
+
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+
+
+echo "=> Starting vsftpd..."
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
